a9d2baeffde5b74c3d2df71d105cf42cfb1e9d1d
1n7 <im.in.cube@gmail.com>
2021-06-22 19:41:51 +0300

    Initial commit

---
+++ android/src/com/mygdx/shitman/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.shitman;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.shitman.ShitMan;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ShitMan(), config);
+	}
+}
